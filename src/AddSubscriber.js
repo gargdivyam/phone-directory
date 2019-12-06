102ff3aea832b472b3b18b0f7e90049ad8a878c6
gargdivyam327 <gargdivyam327@gmail.com>
2019-12-06 23:33:13 +0530

    Routing: Added routing for Add Subscriber page and Show Subscribers page

--- src/AddSubscriber.js
+++ src/AddSubscriber.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import './AddSubscriber.css';
+import { Link } from 'react-router-dom';
+
 
 class AddSusbscriber extends Component {
     
@@ -23,6 +25,8 @@ class AddSusbscriber extends Component {
         e.preventDefault();
         this.props.addSubscriberHandler(this.state);
         this.setState({ id: 0, name: '', phone: ' ' });
+        this.props.history.push("/");
+
     }
 
     render() {
@@ -35,6 +39,9 @@ class AddSusbscriber extends Component {
                 <Header heading="Add Subscriber" />
                 <div className="component-body-container">
                     <button className="custom-btn">Back</button>
+                    <Link to="/">
+                        <button className="custom-btn">Back</button>
+                    </Link>
 
                     <form className="subscriber-form" onSubmit={this.onFormSubmitted.bind(this)}>
                         <label htmlFor="name" className="label-control">Name: </label><br />
