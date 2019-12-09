fd81fbc6d1ecd450bd1bea6708a9c7654e9bfe67
gargdivyam327 <gargdivyam327@gmail.com>
2019-12-09 15:00:08 +0530

    Updates

---
+++ precache-manifest.9defaa3004c4c8a856f5c1e7e2cb946f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "35b912b765ed207c54e8",
+    "url": "/phone-directory/static/js/runtime~main.e30a5ef0.js"
+  },
+  {
+    "revision": "c2b73bb74d29a4aff83d",
+    "url": "/phone-directory/static/js/main.96ff01c6.chunk.js"
+  },
+  {
+    "revision": "ff82c14c6c9f7ba1f615",
+    "url": "/phone-directory/static/js/2.9ee0d791.chunk.js"
+  },
+  {
+    "revision": "c2b73bb74d29a4aff83d",
+    "url": "/phone-directory/static/css/main.fd5634d3.chunk.css"
+  },
+  {
+    "revision": "66158e5875334255fe8dc9855a054fb8",
+    "url": "/phone-directory/index.html"
+  }
+];
